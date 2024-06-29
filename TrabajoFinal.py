70acc5562daf544cef469b093860e0e2ca4f215d
jacquiets <93540360+jacquiets@users.noreply.github.com>
2024-06-29 13:34:56 -0500

    Fix: Lectura archivo

--- TrabajoFinal.py
+++ TrabajoFinal.py
@@ -8,8 +8,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from google.colab import drive
 
-
-#Funcion para determinar el numero de clusters con el metodo del codo
 def metodo_codo(data):
   # Determinación del número óptimo de clusters con el método del codo
     sse = []
@@ -27,7 +25,6 @@ def metodo_codo(data):
     plt.title('Método del Codo para encontrar el k óptimo')
     plt.show()
 
-#Funcion para leer un archivo csv que esta en drive y cargarlo a un dataframe
 def leer_csv_a_dataframe(ruta_archivo, nombre_archivo, columnas):
     """
     Lee un archivo CSV y lo convierte en un DataFrame de pandas con columnas específicas.
@@ -44,9 +41,8 @@ def leer_csv_a_dataframe(ruta_archivo, nombre_archivo, columnas):
         link = ruta_archivo
         # Recuperar archivo de google drive
         !gdown --id {link.split("/")[-2]}
-        df = pd.read_csv('dataTest.csv')
         # Leer el archivo CSV con las columnas especificadas
-        df = pd.read_csv(nombre_archivo, usecols=columnas)
+        df = pd.read_csv(nombre_archivo, usecols=columnas, encoding='latin-1')
         print("Archivo CSV leído exitosamente.")
         return df
     except FileNotFoundError:
@@ -59,41 +55,45 @@ def leer_csv_a_dataframe(ruta_archivo, nombre_archivo, columnas):
         print(f"Error inesperado: {e}")
 
 
-link_drive = 'https://drive.google.com/file/d/105IsGOhI1EV0FC1FJ9mSkyy8LMgB7Fck/view?usp=sharing'
-nombre_archivo = 'dataTest.csv'
-columnas = ['C10_NOMBRE', 'DEPARTAMENTO', 'EDAD', 'SEXO'] 
+link_drive = 'https://drive.google.com/file/d/1V1fo2b_DVUhfhU5HeTwV_Wg94HGR6hEt/view?usp=sharing'
+nombre_archivo = 'dataSIS.csv'
+columnas = ['C10_NOMBRE', 'DEPARTAMENTO', 'EDAD', 'SEXO']
 
 df = leer_csv_a_dataframe(link_drive,nombre_archivo,columnas)
 
-# Preprocesamiento
-# Convertir variables categóricas a numéricas
-label_encoder = LabelEncoder()
-df['SEXO'] = label_encoder.fit_transform(df['SEXO'])
-df['C10_NOMBRE'] = label_encoder.fit_transform(df['C10_NOMBRE'])
-df['DEPARTAMENTO'] = label_encoder.fit_transform(df['DEPARTAMENTO'])
+#Si el dataframe no es nulo
+if df is not None:
+  # Preprocesamiento
+  # Convertir variables categóricas a numéricas
+  label_encoder = LabelEncoder()
+  df['SEXO'] = label_encoder.fit_transform(df['SEXO'])
+  df['C10_NOMBRE'] = label_encoder.fit_transform(df['C10_NOMBRE'])
+  df['DEPARTAMENTO'] = label_encoder.fit_transform(df['DEPARTAMENTO'])
 
 
-# Seleccionar características para el clustering
-X = df[['C10_NOMBRE', 'EDAD','DEPARTAMENTO','SEXO']]
 
-# Escalar los datos
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+  # Seleccionar características para el clustering
+  X = df[['C10_NOMBRE', 'EDAD','DEPARTAMENTO','SEXO']]
 
-# Aplicar K-means
-#Numero de clusters de prueba
-kmeans = KMeans(n_clusters=8, random_state=42)
-kmeans.fit(X_scaled)
+  metodo_codo(df)
 
-# Añadir los labels al DataFrame original
-df['Cluster'] = kmeans.labels_
+  # Escalar los datos
+  scaler = StandardScaler()
+  X_scaled = scaler.fit_transform(X)
 
-# Visualización
-plt.figure(figsize=(10, 6))
-sns.scatterplot(data=df, x='EDAD', y='C10_NOMBRE', hue='Cluster', palette='viridis')
-plt.title('K-means Clustering de Pacientes Oncológicos')
-plt.xlabel('Edad')
-plt.ylabel('Diagnostico')
-plt.show()
+  # Aplicar K-means
+  kmeans = KMeans(n_clusters=3, random_state=50)
+  kmeans.fit(X_scaled)
 
-# print(df)
+  # Añadir los labels al DataFrame original
+  df['Cluster'] = kmeans.labels_
+
+  # Visualización
+  plt.figure(figsize=(10, 6))
+  sns.scatterplot(data=df, x='EDAD', y='DEPARTAMENTO', hue='Cluster', palette='viridis')
+  plt.title('K-means Clustering de Pacientes Oncológicos')
+  plt.xlabel('Edad')
+  plt.ylabel('Diagnostico')
+  plt.show()
+
+  # print(df)
